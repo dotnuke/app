64e8422e359c60623d83c6a24ff23b3f7449d94c
hai <thanhhai_kg@yahoo.com>
2013-10-21 09:07:14 +0700

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bvkienluong::Application.config.secret_token = '3444384bc941d9cb576938d5c3f54a837a879d983aa6f005379b6b1c4ac1ea45d7197fde812a6da7b7b1affcf0211e7e27c8201734d4f1859adfcee1725f4764'
